cc663c3d3c363b851ea20635195d7bf2ae3996d4
cargnieli2016 <cargnieli2016@gmail.com>
2022-08-03 14:05:07 -0300

    Iniciando com TypeScript

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+
+  nome: string = 'Terra';
+  temperatura: number = 15;
+  diametro: number = 12.742;
+  dia: number = 24;
+  ano : number = 365;
+  curiosidade: string = 'Daqui há 140 milhões de anos, um dia durará 25 horas!';
+
   ngOnInit() {}
 }
