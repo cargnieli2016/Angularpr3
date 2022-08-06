ec93721458faabd2316548fb1a834663351ee81c
cargnieli2016 <cargnieli2016@gmail.com>
2022-08-06 08:23:55 -0300

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,12 +11,16 @@ export class AppComponent {
   planeta: Planeta = {
     nome: '',
     temperatura: 15,
-    diametro: 12.742,
+    diametro: 12742,
     dia: 24,
     ano: 365,
     curiosidade: 'Daqui há 140 milhões de anos, um dia durará 25 horas!'
-
   };
+
+palavras: string[] = ['teste1', 'teste2', 'teste3'];
+numeros:  number[] = [99, 55, 88];
+
+
   
   ngOnInit() {}
   ClicouNoPlaneta(){
